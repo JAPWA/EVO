ea5ed9b24b8ad79f423b2058c34ef3319af9d34a
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-08-17 22:32:59 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -2808,12 +2808,12 @@ local Text = [[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '(YOUTUBE BOT),url="t.me/Ssociial_bot"}},
+{{text = '(JABWA),url="t.me/Ssociial_bot"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/sssstaaar/8&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
-if text == 'المبرمج فامبير' then
+if text == 'المبرمج  ايفانيي' then
 local Text = [[
 المبرمج فامبير لو حابب تتواصل معاهه
 ]]
