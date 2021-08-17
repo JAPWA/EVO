e5bf8ee5ad74b1e53dacb3383d5efd844d057cf0
JAPWA <88171896+JAPWA@users.noreply.github.com>
2021-08-17 22:56:30 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -3347,7 +3347,7 @@ keyboard.inline_keyboard = {
 {{text = 'SOURCE CHANNEL ', url="t.me/SOURCE_STAR1"}},
 }  
 local msg_id = msg.id_/2097152/0.5  
-https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo=https://t.me/sssstaaar/9&caption=  .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))  
 end
 if text == "استوري" or text == "ستوري" or text == "فيديوهات" and not  database:get(bot_id.."sing:for:me"..msg.chat_id_) then 
 ght = math.random(2,22); 
